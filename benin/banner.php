e17c50bec567db1264a666c0333e562e75676656
fluriellecodjia <70434194+fluriellecodjia@users.noreply.github.com>
2020-09-21 19:00:01 +0100

    Add files via upload

---
+++ benin/banner.php
@@ -0,0 +1,18 @@
+<div class="tata">
+        <h2>SOMMAIRE</h2>
+    
+        <ol>
+            <li><A href="#Géographie">Géographie</A>
+                <ol>
+                    <li><A href="#Localisation">Localisation et frontières</A></li>
+                    <li><A href="#Relief">Relief</A> </li>
+                </ol>
+            </li>
+            <li><A href="#Politique">Politique et administration</A>
+                <ol>
+                    <li><A href="#Découpage">Découpage territorial</A></li>
+                </ol>
+            </li>
+            <li><A href="#Culture">Culture</A> </li>
+        </ol> 
+    </div>
